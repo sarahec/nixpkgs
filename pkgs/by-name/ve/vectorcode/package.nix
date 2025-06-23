cd2ad772f8c2339854bcd6fb10811b65a414e662
Sarah Clark <seclark@nextquestion.net>
2025-06-23 12:11:41 -0700

    vectorcode: add bundled chromadb to path

--- pkgs/by-name/ve/vectorcode/package.nix
+++ pkgs/by-name/ve/vectorcode/package.nix
@@ -112,6 +112,16 @@ python.pkgs.buildPythonApplication rec {
     installShellCompletion vectorcode.{bash,zsh}
   '';
 
+  postFixup = ''
+    wrapProgram $out/bin/vectorcode \
+      --prefix PYTHONPATH : "$PYTHONPATH" \
+      --set PATH ${
+        lib.makeBinPath [
+          python
+        ]
+      };
+  '';
+
   pythonImportsCheck = [ "vectorcode" ];
 
   nativeCheckInputs =
